a7736dd13f57b927754a623d3ba7cebc76f44ccf
Anna Garren <35542357+aggarren@users.noreply.github.com>
2022-07-11 23:46:33 -0400

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDED_constants
+#define INCLUDED_constants
+#define MAXCAP 10 //used for initial number of maximum MyStrings in a StringList
+#define EOS '\0' //end of string character
+#define EOL '\n' //end of line character
+#define DEFAULT_ARRAY_SIZE 10 //used for creation of StringList's array
+#endif
