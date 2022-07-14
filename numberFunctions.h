7f62ceb3099ac27a60da2aeac37f6dd08858db31
Anna Garren <35542357+aggarren@users.noreply.github.com>
2022-07-14 18:52:49 -0400

    Create numberFunctions.h

---
+++ numberFunctions.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDED_numberFunctions
+#define INCLUDED_numberFunctions
+//PRE: IS is the number to be stored
+//POST OS is the number
+int readNumber();
+
+//PRE: parameters include the base and power
+//POST: outputs the base to the power of the exponent
+int pow(int base, int exponent);
+
+#endif
