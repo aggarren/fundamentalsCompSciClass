7ad26d95f7b3ef017311f202a9f409ee18ea5cb8
Anna Garren <35542357+aggarren@users.noreply.github.com>
2022-07-14 18:51:52 -0400

    Create arithmetic.h

---
+++ arithmetic.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDED_arithmetic
+#define INCLUDED_arithmetic
+//PRE: IS includes the operators, an array to store the appropriate number of integares, and the length of the operator array
+//POST: OS will compute the output 
+int arithmetic(char operators[], int numbers[], int maxOp);
+#endif
