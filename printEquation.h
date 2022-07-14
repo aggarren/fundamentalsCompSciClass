5e8c97676ced4e5f97b583f1b3daf1523452992e
Anna Garren <35542357+aggarren@users.noreply.github.com>
2022-07-14 18:47:47 -0400

    Create printEquation.h

---
+++ printEquation.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDED_printEquation
+#endif
+//PRE: IS includes the operators and the numbers that satisfy the equation and the result
+//POST: OS prints the equation using the operators, numbers, and result correctly
+void printEquation(char operators[], int numberArray[], int result, int maxOp);
+       
